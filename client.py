671110cf7c39b44c5dd46b7d722788437471f72e
Otávio Thomas Bertucini <otaviobertucini@gmail.com>
2021-10-21 21:48:11 -0300

    Client reference done.

--- client.py
+++ client.py
@@ -2,19 +2,35 @@ import Pyro4
 import threading
 import sys
 
+# chave https://medium.com/@jonathas.mpf/assinatura-digital-com-python-d03df25116fb
+# from Crypto.Hash import SHA256
+# from Crypto.PublicKey import RSA
+# from Crypto import Random
+
 Running = True
+# random_seed = Random.new().read
+# keyPair = RSA.generate(1024, random_seed)
+# pubKey = keyPair.publickey()
+
+menu_message = 'Digite 1 para cadastrar usuário\nDigite 2 para cadastrar nova enquete\nDigite 3 para votar em uma enquete'
 
 @Pyro4.expose
 @Pyro4.callback
 class Client(object):
 
+    @Pyro4.expose
     def notification(self):
         print("callback recebido do server!")   
 
     def loopThread(daemon):
         # thread para ficar escutando chamadas de método do server
-        print('Cliente esperando')
+        print('Menu')
+        print(menu_message)
         daemon.requestLoop(lambda: Running)
+
+    def callBackLoopThread(objetc):
+        # thread->requestLoop do callback
+        print('sei não ehin')
 
 def main():
 
@@ -27,8 +43,9 @@ def main():
     server.test()
     daemon = Pyro4.core.Daemon()
     callback = Client()
-    daemon.register(callback)
+    client_uri = daemon.register(callback)
     # loopThread = callback.loopThread
+    callback.callBackLoopThread()
     # inicializa a thread para receber notificações do server
     thread = threading.Thread(target=Client.loopThread, args=(daemon,))
     thread.daemon = False
@@ -38,7 +55,13 @@ def main():
             global Running
             Running = False
             break
-        print('Digite exit para sair')
+        if '2' == line.rstrip():
+            pool_name = input('Nome da enquete: ')
+            server.register(client_uri, pool_name, None)
+            print('Enquete criada: ' + str(pool_name))
+        if '3' == line.rstrip():
+            server.getClients()
+        print(menu_message)
     print('Exit')
 
 main()
--- server.py
+++ server.py
@@ -5,7 +5,21 @@ import Pyro4
 
 @Pyro4.expose
 @Pyro4.behavior(instance_mode="single")
+
+class ClientInstance:
+
+    name = ''
+    referece = ''
+    publicKey = ''
+
+    def __init__(self, name, reference, key):
+
+        self.name = name
+        self.referece = reference
+        self.publicKey = key
 class Server(object):
+
+    clients = []
 
     #coesão e desacoplamento------------------------       MÃO DO SIMÃO
     def runServer(self):                              #$$$$$$$$$$$$$$$$$$$$$$$$$
@@ -14,13 +28,37 @@ class Server(object):
             uri = daemon.register(self)               #$$$$$$$'/ $/ `/ `$' .$$$$
             ns = Pyro4.locateNS()                     #$$$$$$'|. i  i  /! .$$$$$
             ns.register("SearchNameServer", uri)      #$$$$$$$'_'.--'--'  $$$$$$        
+            print('uri:', uri)
             print("Server is ready")                  #$$^^$$$$$'        J$$$$$$
             daemon.requestLoop()                      #$$$   ~""   `.   .$$$$$$$
                                                       #$$$$$',      ;  '$$$$$$$$                                                
                                                       #$$$$$$$$$$$.'   $$$$$$$$$
+
+    @Pyro4.expose    
+
     def test(self):                                 
 
         print("Não sei de nada")
+
+    @Pyro4.expose
+    def register(self, uri, name, key):
+        # registra usuário, passa chave e referência do cliente
+        print(uri)
+        client = Pyro4.Proxy(uri)
+
+        instance = ClientInstance(name, client, key)
+        self.clients.append(instance)     
+
+    def poolResquest(self):
+        # chamar método do cliente para avisar nova enquete
+        print('nada')
+
+    @Pyro4.expose
+    def getClients(self):
+
+        for client in self.clients:
+
+            print('oie: ' + client.name)
 
 def main():
     server = Server()
