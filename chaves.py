b3bb0452769ae4d8e860e7e4145457d9dec60ef4
Saulo Bergamo <saulobergamo@hotmail.com>
2021-10-26 19:29:45 -0300

    chaves e hash que não funcionam

---
+++ chaves.py
@@ -0,0 +1,32 @@
+from Crypto.Hash import SHA256
+from Crypto.PublicKey import RSA
+from Crypto import Random
+
+random_seed = Random.new().read
+
+keyPair = RSA.generate(1024, random_seed)
+pubKey = keyPair.publickey()
+
+True_text = 'Hello Bob'
+Fake_text = 'Bye Bob'
+
+hashA = SHA256.new(True_text.encode('utf-8')).digest()
+digitalSign = keyPair.sign(hashA, '')
+
+print("HashA:" + repr(hashA) + "\n")
+print("Digital signature:" + repr(digitalSign) + "\n")
+
+hashB = SHA256.new(True_text.encode('utf-8')).digest()
+hashC = SHA256.new(Fake_text.encode('utf-8')).digest()
+
+print("HasCB:" + repr(hashB) + "\n")
+print("HashCA:" + repr(hashC) + "\n")
+
+if pubKey.verify(hashB, digitalSign):
+    print("O texto autêntico é" + True_text)
+    
+elif pubKey.verify(hashB, digitalSign):
+    print("O texto autêntico é" + Fake_text)
+    
+else:
+    print("Nenhum dos textos é autêntico")
