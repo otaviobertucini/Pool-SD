494796d33ef9272ce9d45c15fb28295c456f71f7
Otávio Thomas Bertucini <otaviobertucini@gmail.com>
2021-11-23 20:59:18 -0300

    So far so good.

--- REST/Client/src/App.js
+++ REST/Client/src/App.js
@@ -1,37 +1,8 @@
-import React from "react";
-
-let HAS_LOADED = false;
+import React, { useState } from "react";
+import Main from "./main";
 
 function App() {
-  function start() {
-    // Subscribe no SSE
-    var source = new EventSource("http://127.0.0.1:8000/poll");
-
-    source.addEventListener("new", (event) => {
-      // console.log("rsrs ", event);
-    });
-  }
-
-  if (!HAS_LOADED) {
-    HAS_LOADED = true;
-
-    const name = prompt("Digite o nome do cliente");
-
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open("POST", "http://127.0.0.1:8000/poll/"+name, true);
-    xmlHttp.setRequestHeader("Access-Control-Allow-Origin", "*");
-    xmlHttp.onload = function (event) {
-      alert("Deu boa");
-      start();
-    };
-    xmlHttp.send(
-      JSON.stringify({
-        name,
-      })
-    );
-  }
-
-  return <>Oi</>;
+  return <Main></Main>;;
 }
 
 export default App;
